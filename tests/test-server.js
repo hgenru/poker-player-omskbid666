6f12dbd96b21a3dc515f09ffe46469897438ba56
Nikita Levchuk <nikita@Nikitas-MacBook-Pro.local>
2016-04-09 16:59:11 +0600

    test post request

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,11 +1,19 @@
 'use strict';
 const request = require('supertest');
 const app = require('../player_service');
+const example_json = require('./assets/tournament.json');
 
 describe('server', function() {
     it('server is ok', function() {
-        request(app)
+        return request(app)
             .get('/')
             .expect(200);
+    });
+
+    it('server is return digit', function() {
+        return request(app)
+           	.post('/')
+           	.send({ action: 'bet_request', game_state: JSON.stringify(example_json) })
+            .expect(200, '10000');
     });
 });
