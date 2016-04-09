6d7a2988fee4548da91c2d3ee5912d971d4b180d
Александр Плесовских <me@hgen.ru>
2016-04-09 12:47:54 +0500

    3st

--- player.js
+++ player.js
@@ -4,7 +4,7 @@ module.exports = {
   VERSION: "JavaScript Omsk Bird (простите, у меня не работает клава нормально)",
 
   bet_request: function(game_state, bet) {
-    bet(50);
+    bet(10000);
   },
 
   showdown: function(game_state) {
