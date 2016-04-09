32fd61faec0b50447219acfc3784ba26e213db5a
Александр Плесовских <me@hgen.ru>
2016-04-09 13:58:28 +0500

    fuck strict

--- player.js
+++ player.js
@@ -1,6 +1,48 @@
-const CARD_RANKS = {
+'use strict';
 
+const CARD_RANKS = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 11,
+    'J': 12,
+    'Q': 13,
+    'K': 14,
+    'A': 16
 };
+
+const GAME_CASES = [
+    (c1, c2) => c1.rank === c2.rank ? 10 : 0,
+    (c1, c2) => c1.suit === c2.suit ? 4 : 0,
+    (c1, c2) => {
+        let points = {1: 3, 2: 2, 3: 1};
+        let distance = Math.abs(c1.rank - c2.rank);
+        return points[distance] || 0;
+    }
+];
+
+const HUTCHINSON_CONST = 30;
+// const HUTCHINSON_CONST = 34;
+
+function raiseCheck(cards) {
+    for (let card of cards) {
+        card.rank = CARD_RANKS[card.rank];
+    }
+    let points = 0;
+    for (let gameCaseFunc of GAME_CASES) {
+        points += gameCaseFunc(cards[0], cards[1]) || 0;
+    }
+    if (points >= HUTCHINSON_CONST) {
+        return true;
+    }
+    return false;
+}
 
 module.exports = {
 
@@ -12,8 +54,11 @@ module.exports = {
                 return p.hole_cards && p.hole_cards.length > 0;
             });
             let myCards = player.hole_cards;
-            // myCards.
-
+            let iCanRaise = raiseCheck(myCards);
+            if (iCanRaise) {
+                return bet(10000);
+            }
+            return bet(0);
         } catch (e) {
             bet(10000);
         }
