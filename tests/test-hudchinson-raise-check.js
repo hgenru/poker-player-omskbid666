a59b83d49c89bf0a4cf49a48e11d57012bc7aac3
Александр Плесовских <me@hgen.ru>
2016-04-09 14:19:47 +0500

    add tests

---
+++ tests/test-hudchinson-raise-check.js
@@ -0,0 +1,35 @@
+'use strict';
+const chai = require('chai'); chai.should();
+const raiseChech = require('../player').raiseCheck;
+
+describe('hudchinson', function() {
+    describe('raise chech', function() {
+        it('shoud not raise', function() {
+            const HAND = [
+                {
+                    rank: '6',
+                    suit: 'hearts'
+                },
+                {
+                    rank: 'K',
+                    suit: 'spades'
+                }
+            ];
+            raiseChech(HAND).should.be.equal(false);
+        });
+
+        it('shoud raise', function() {
+            const HAND = [
+                {
+                    rank: 'A',
+                    suit: 'hearts'
+                },
+                {
+                    rank: 'K',
+                    suit: 'spades'
+                }
+            ];
+            raiseChech(HAND).should.be.equal(true);
+        });
+    });
+});
